24494a278e6e6c8241818988733ead2816cdb09c
Paulcheeba <75949480+paulcheeba@users.noreply.github.com>
2025-09-16 22:57:49 -0600

    Create io.js

---
+++ scripts/io.js
@@ -0,0 +1,17 @@
+/* global saveDataToFile, readTextFromFile */
+import { State } from "./state.js";
+
+export class IO {
+  static async exportJSON() {
+    const state = game.settings.get("fvtt-treasury", "state");
+    const stamp = new Date().toISOString().replace(/[:.]/g,"-");
+    const name = `fvtt-treasury-${stamp}.json`;
+    await saveDataToFile(JSON.stringify(state, null, 2), "application/json", name);
+  }
+
+  static async importJSON(file) {
+    const text = await readTextFromFile(file);
+    const parsed = JSON.parse(text);
+    await State.mutate("import-json", { state: parsed });
+  }
+}
